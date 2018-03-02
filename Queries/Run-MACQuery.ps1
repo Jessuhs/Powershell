370d75304e8b74162acc30c8d7ae7c2efa0f1106
Jessuhs <36280738+Jessuhs@users.noreply.github.com>
2018-03-02 11:28:59 -0600

    Update Run-MACQuery.ps1

--- Queries/Run-MACQuery.ps1
+++ Queries/Run-MACQuery.ps1
@@ -8,18 +8,22 @@ While ($strQuit -eq "Y")
     [string[]]$Computers = (Read-Host "`Please enter the computer names (separate with comma)").split(",")
 
     #This function generates a list of Computer Names, their corresponding network adapters, and those adapters' MAC addresses,
+
+    $report = @()
     function Run-MACQuery($Computer)
     {   
         Get-CimInstance -Class Win32_NetworkAdapterConfiguration -ComputerName $Computer |
-        Where-Object {$_.IpAddress.Count -ge 1} |
-        Format-Table @{l='ComputerName';e={$_.DNSHostName}},@{l='Description';e={$_.Description}}, @{l='MAC Address';e={$_.MACAddress}} -AutoSize
+        Where-Object {$_.IpAddress.Count -ge 1}
     }
         
-    #Walk through each PC entered by user and send it to the function above for processing
+    #Step through each PC entered by user and send it to the function above for processing
     ForEach ($Computer in $Computers)
     {
-        Run-MACQuery($Computer)
+        $report += Run-MACQuery($Computer)
     }
+    
+    #Output the table in a grid view
+    $report | Select-Object @{l='ComputerName';e={$_.DNSHostName}},@{l='Description';e={$_.Description}}, @{l='MAC Address';e={$_.MACAddress}} | Out-GridView
     
     #Prompt the user to continue or break the loop
     $strQuit = Read-Host "Would you like to search again? (Y/N)"
